f24ce79666e924f296c7fa8c822b01af1cedb006
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2022-01-28 00:13:24 +0530

    Added basic content

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//JS content
+let nav = 0;                 //display current month looking at
+let clickable = null;        //display date of a month when clicked
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];  //display array of objects
+
+const calendar = document.getElementById('calendar');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
